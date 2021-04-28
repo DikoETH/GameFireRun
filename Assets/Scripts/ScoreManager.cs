423f49d970c16b9544324c4ed56f82f16cf1e109
StasUzumaki <stasl77775@gmail.com>
2021-04-28 17:22:50 +0300

    Score Script

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highscoreText;
+    private float score;
+     int highscore;
+     public float total;
+
+
+    void Start()
+    {
+        score =0;
+
+    }
+
+
+    void Update()
+    {
+
+        score +=1 * Time.deltaTime;
+        scoreText.text = ((int)score).ToString();
+
+        highscore =(int)score;
+        scoreText.text =" " + highscore.ToString();
+        if (PlayerPrefs.GetInt("score") <= highscore)
+        PlayerPrefs.SetInt("score", highscore);
+
+        highscoreText.text = " " + PlayerPrefs.GetInt("score").ToString(); 
+       
+
+            
+           
+    } 
+
+}
