563ce0622e89ad3ffc3dc376c19088e7b7238ce1
QuariX <antonim.n1.a@gmail.com>
2021-05-09 15:14:58 +0300

    add shop and money counter

---
+++ Assets/Scripts/MoneyText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyText : MonoBehaviour
+{
+    public static int Money;
+    Text text;
+
+    void Start(){
+        text = GetComponent<Text>();
+        /*Money = PlayerPrefs.GetInt("money");*/
+    }
+
+    void Update(){
+        text.text = Money.ToString();
+        /*Money = PlayerPrefs.SetInt();*/
+    }
+}
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    private int index;
+    [SerializeField] private SetSkin setSkin;
+    [SerializeField] private Text priceText;
+    [SerializeField] private int[] price;
+
+    private void Start(){
+        index = setSkin.index;
+        /*priceText.text = price[index].ToString();*/
+        DetectionPriceSkin();
+        
+    }
+
+    public void SkinMinus(){
+        if(index > 0){
+            index--;
+        }
+        else
+        {
+           index = 0; 
+        }
+        setSkin.UpdateSkin(index);
+        DetectionPriceSkin();
+        PlayerPrefs.SetInt("index", index);
+    }
+
+    public void SkinPlus(){
+        if(index < 5){
+            index++;
+        }
+        else
+        {
+           index = 5; 
+        }
+        setSkin.UpdateSkin(index);
+        DetectionPriceSkin();
+        PlayerPrefs.SetInt("index", index);
+    }
+
+    private void DetectionPriceSkin(){
+        if(MoneyText.Money >= price[index]){
+            priceText.color = Color.green;
+        }
+        else if(MoneyText.Money < price[index])
+        {
+            priceText.color = Color.red;
+        }
+    }
+
+    public void BuySkin(){
+    }
+}
